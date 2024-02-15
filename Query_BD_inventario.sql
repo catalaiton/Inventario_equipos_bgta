a852ad50cb99b41b5f04c9d6276a5d1427b951ef
Catalina <lclaitonp@udistrital.edu.co>
2024-02-15 12:08:30 -0500

    Modificaciones en las tablas de la BD

--- Query_BD_inventario.sql
+++ Query_BD_inventario.sql
@@ -19,22 +19,24 @@ select * from sede;
 
 CREATE TABLE equipo(
 	serial_equipo int not null,
+	n_maquina varchar(50),
+    estado varchar(50),
+    propiedad varchar(50),
     contrato int not null,
     marca Varchar(50),
     orden_interna int,
     cuenta int,
-    linea_marca varchar(50),
     modelo varchar(50),
+    linea_marca varchar(50),
     procesador varchar(50),
     disco varchar(50),
     memoria varchar(50),
     software_ad varchar(50),
-    n_maquina varchar(50),
+    tipo_equipo varchar(50),
     f_activacion date,
     f_finalizacion date,
-    estado varchar(50),
-    propiedad varchar(50),
-    tipo_equipo varchar(50),
+    f_entrega date,
+    doc_usuarioant int,
     cod_sede int,
     primary key (serial_equipo),
     foreign key(cod_sede) references sede(codigo)
@@ -47,7 +49,6 @@ select * from equipo;
 CREATE TABLE analista (
 	documento int not null,
     nombre Varchar(100) not null,
-    contacto varchar(50),
     cod_sede int,
     primary key(documento),
     foreign key (cod_sede) references sede(codigo)
